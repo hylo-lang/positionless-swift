483990fbabe1e7ffa10513c1ef368a29d5578655
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2025-06-07 03:24:11 +0530

    added abstraction for mutable collection

--- Sources/Positionless/Positionless.swift
+++ Sources/Positionless/Positionless.swift
@@ -116,6 +116,33 @@ extension Collection {
 
 }
 
+/// Mutable partitions of collection.
+protocol MutableCollectionPartition: CollectionPartition
+where Part: MutableCollection {
+
+  /// Swaps first element partition i and j.
+  ///
+  /// - Precondition: `i >= 0 && j >= 0 && i < partitionCount && j < partitionCount`.
+  mutating func swapFirst(_ i: Int, _ j: Int)
+
+  /// The parts.
+  ///
+  /// Invariant: `parts.count() == partitionCount`
+  var parts: [Part] { get set }
+
+}
+
+/// A collection that supports mutation of elements.
+protocol MutableCollection: Collection
+where Partition: MutableCollectionPartition {
+
+  /// The first element of the collection.
+  ///
+  /// - Precondition: !self.isEmpty()
+  var first: Element { get set }
+
+}
+
 /// A collection such as a deque, with an internally partitioned
 /// structure.
 ///
