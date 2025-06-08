a5859228b5e7bc33582222ebdf9126542098ec14
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2025-06-08 16:30:35 +0530

    added fixed array

---
+++ Sources/Positionless/FixedArray.swift
@@ -0,0 +1,19 @@
+/// An array whose shape can't be changed. Only elements are mutable.
+struct FixedArray<Element> {
+  /// Actual storage of elements.
+  private var storage: [Element]
+
+  /// Creates an instance from given array.
+  public init(_ storage: [Element]) {
+    self.storage = storage
+  }
+
+  /// Number of elements in array.
+  public var count: Int { storage.count }
+
+  /// Access individual elements.
+  public subscript(index: Int) -> Element {
+    get { storage[index] }
+    set { storage[index] = newValue }
+  }
+}
