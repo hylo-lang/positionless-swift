f8cb93e10e625a126a26cc085df8d237681b5403
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2025-08-12 21:27:30 +0530

    slice firstier implementation of reverse

--- Sources/Positionless/Algorithms/Reverse.swift
+++ Sources/Positionless/Algorithms/Reverse.swift
@@ -1,19 +1,13 @@
-extension MutableCollection where Self: BidirectionalCollection {
+extension MutableBidirectionalCollection {
   /// Reverses the order of elements in `self`.
   ///
   /// - Complexity: O(n)
   mutating func reverse_() {  // FIXME: _ after reverse is to avoid ambiguity with stdlib reverse.
-    // Start with [ _ | _ | a b c d e ]
-    withMutableParts(count: 3) { p in
-      p.transferAllToPrev(from: 2)  // --> [ _ | a b c d e | _ ]
-
-      // Loop Invariant:
-      // Every iteration of loop appends last element of `p[1]` to `p[0]` and
-      // prepends first element of `p[1]` to `p[2]`.
-      while p[part: 1].count > 1 {
-        p.shrink(part: 1)  // --> [ _ | a b c d | e ]
-        p.swapFirst(1, 2)  // --> [ _ | e b c d | a ]
-        p.grow(part: 0)  // --> [ e | b c d | a ]
+    withMutableSlice { rest in
+      while rest.count > 1 {
+        rest.swapEnds()
+        _ = rest.dropFirst()
+        _ = rest.dropLast()
       }
     }
   }
