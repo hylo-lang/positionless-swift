e0033254e8737cb880580e5a594c8f597d9c0795
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2025-08-19 00:08:26 +0530

    implement find_if algorithm for positionless design

---
+++ Sources/Positionless/Algorithms/Searching.swift
@@ -0,0 +1,43 @@
+extension Collection {
+  /// Splits the collection at the first element that satisfies the given predicate.
+  ///
+  /// - Postcondition:
+  ///   - `part[0]` contains all elements before the first match.
+  ///   - `part[1]` contains the first matching element and all elements after it.
+  ///
+  /// - Complexity: O(`count`).
+  func splitFirstWhere<R>(where predicate: (Element) -> Bool, _ f: (inout Parts) -> R) -> R {
+    return withParts(count: 2) { p in
+      while !p[part: 1].isEmpty() {
+        if predicate(p[part: 1].first) {
+          break
+        }
+        p.grow(part: 0)
+      }
+      return f(&p)
+    }
+  }
+}
+
+extension MutableCollection {
+  /// Splits the collection at the first element that satisfies the given predicate.
+  ///
+  /// - Postcondition:
+  ///   - `part[0]` contains all elements before the first match.
+  ///   - `part[1]` contains the first matching element and all elements after it.
+  ///
+  /// - Complexity: O(`count`).
+  mutating func splitFirstWhereMut<R>(where predicate: (Element) -> Bool, _ f: (inout Parts) -> R)
+    -> R
+  {
+    return withMutableParts(count: 2) { p in
+      while !p[part: 1].isEmpty() {
+        if predicate(p[part: 1].first) {
+          break
+        }
+        p.grow(part: 0)
+      }
+      return f(&p)
+    }
+  }
+}
